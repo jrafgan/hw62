fb2c67327ab26c1eb69012839491122dcabd18ec
Maksat Duishenaliev <maksatovy@gmail.com>
2019-01-28 22:11:07 +0600

    Prettifyed and re-checked

--- src/components/elements/footer/RightColumn.js
+++ src/components/elements/footer/RightColumn.js
@@ -3,7 +3,7 @@ import React from 'react';
 const RightColumn = () => {
     return (
         <div className='footer_right_col'>
-           RightColumn
+           Some more Info here
         </div>
     );
 };
--- src/components/elements/header/NavMenu.js
+++ src/components/elements/header/NavMenu.js
@@ -6,25 +6,25 @@ class NavMenu extends Component {
         showMenu: false,
     };
 
-    toggleMenu=()=>{
+    toggleMenu = () => {
         !this.state.showMenu ? this.setState({showMenu: true}) : this.setState({showMenu: false});
-        console.log(this.state.showMenu);
     };
-    render() {
-    return (
-        <Fragment>
-            <div className='NavMenu'>
-                <button type='button' className='menu_btn' onClick={this.toggleMenu}>Menu</button>
-                {<ul className={this.state.showMenu ? 'show' : 'menu_ul'}>
-                    <li className='menu_li'><NavLink exact to="/">Главная</NavLink></li>
-                    <li className='menu_li'><NavLink to="/catalog">Каталог оборудования</NavLink></li>
-                    <li className='menu_li'><NavLink to="/advantage">Преимущество аренды</NavLink></li>
-                    <li className='menu_li'><NavLink to="/delivery">Доставка</NavLink></li>
-                </ul>}
-            </div>
 
-        </Fragment>
-    );
+    render() {
+        return (
+            <Fragment>
+                <div className='NavMenu'>
+                    <button type='button' className='menu_btn' onClick={this.toggleMenu}>Menu</button>
+                    {<ul className={this.state.showMenu ? 'show' : 'menu_ul'}>
+                        <li className='menu_li'><NavLink exact to="/">Главная</NavLink></li>
+                        <li className='menu_li'><NavLink to="/catalog">Каталог оборудования</NavLink></li>
+                        <li className='menu_li'><NavLink to="/advantage">Преимущество аренды</NavLink></li>
+                        <li className='menu_li'><NavLink to="/delivery">Доставка</NavLink></li>
+                    </ul>}
+                </div>
+
+            </Fragment>
+        );
     }
 }
 
